2d4fbf47d70d85392a9fe1087b57c17e92895f8f
Vivek Bhusal <vivekbhusal@gmail.com>
2015-08-21 18:48:36 +1000

    setup database

--- src/wp-config.php
+++ src/wp-config.php
@@ -26,10 +26,10 @@ define('DB_NAME', 'titanium_wp_db');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
