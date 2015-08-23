9655cc5bbf0bdb50f53d73f4b335eeadfe0ebb5b
mingda1992 <mingda1992@hotmail.com>
2015-08-23 14:16:30 +1000

    reposition the menu

--- wp-content/themes/quality/header.php
+++ wp-content/themes/quality/header.php
@@ -57,7 +57,7 @@
                 'container'         => 'div',
                 'container_class'   => 'collapse navbar-collapse',
 				'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
+                'menu_class'        => 'nav navbar-nav titnium-menu-nav',
                 'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                 'walker'            => new wp_bootstrap_navwalker())
             );
