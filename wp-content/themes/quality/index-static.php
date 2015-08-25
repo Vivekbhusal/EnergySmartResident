d8f1175514f5f7620e4670c3786d4608b6e9db10
Rachel0830 <yfu44@student.monash.edu>
2015-08-25 12:32:59 +1000

    change style of team part and slider

--- wp-content/themes/quality/index-static.php
+++ wp-content/themes/quality/index-static.php
@@ -4,12 +4,14 @@ $current_options = wp_parse_args(  get_option( 'quality_pro_options', array() ),
 
 
 <!-- /Quality Main Slider --->
+<div class="slider-input">
 <div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo $current_options['home_feature']; ?>"></div>
 
 <div class="flex-slider-center">
 	<div class="input-group center-block search-suburb">
 		<input type="text" class="form-control" placeholder="Please enter a suburb name..."/>
 	</div>
+</div>
 </div>
 
 <div class="carousel">
