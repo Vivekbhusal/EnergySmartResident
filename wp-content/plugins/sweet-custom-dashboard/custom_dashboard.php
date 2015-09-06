00663ddc63f6bd140fe87765c1528a5443ed3af1
Rachel0830 <yfu44@student.monash.edu>
2015-09-06 20:58:25 +1000

    modify ADD buttons in dashboard

--- wp-content/plugins/sweet-custom-dashboard/custom_dashboard.php
+++ wp-content/plugins/sweet-custom-dashboard/custom_dashboard.php
@@ -16,10 +16,35 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 		<a href=""><span class="logo-photo"/></a>
 	</div>
 <p id="dashboard-p">
-	Energy Smart Resident is build on concept to measure <b>'How green is your neighbourhood?'</b>.<br/>
-	On this user personal page, you are able to add new properties by clicking <a href="/wp-admin/post-new.php?post_type=house">Add New House</a>.<br/>
-	Also, you can make some posts to our blog.
+	Energy Smart Resident is build on the concept to measure <b>'How green is your neighbourhood?'</b>.<br/>
+	On this user personal page, you are able to add new properties by clicking Add House.<br/>
+	Users can edit their account information on <a href="/wp-admin/profile.php">Profile Page</a>.<br/>
+	Also, you can make some posts to our Blog.
 </p>
+	<div class="add-button-row">
+		<div class="add-house">
+			<a href="/wp-admin/post-new.php?post_type=house">
+			<div class="add-house-block">
+				<span>+</span>
+			</div></a>
+			<h2>Add House<h2>
+
+		</div>
+		<div class="add-blog">
+			<a href="/wp-admin/post-new.php">
+			<div class="add-blog-block">
+				<span>+</span>
+			</div></a>
+			<h2>Add Blog</h2>
+
+		</div>
+<!--		<p>-->
+<!--			<h3>You Can:</h3>-->
+<!--			<h4>Add New Property  <a href="/wp-admin/post-new.php?post_type=house"><span class="add"></span></a></h4>-->
+<!--			<h4>Edit Account Information  <a href="/wp-admin/profile.php"><span class="edit"></span></a></h4>-->
+<!--			<h4>Make Posts On The Blog  <a href="/wp-admin/edit.php"><span class="post"></span></a></h4>-->
+<!--		</p>-->
+	</div>
 <!--	Delete till here-->
 
 </div>
