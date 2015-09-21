61d4d39abb253ee9beeed330200cf54189e5e310
Rachel0830 <yfu44@student.monash.edu>
2015-09-21 21:32:27 +1000

    Hight of latest property section changed

--- wp-content/themes/quality/index-aboutus.php
+++ wp-content/themes/quality/index-aboutus.php
@@ -8,7 +8,7 @@
 ?>
 <?php $quality_pro_options=theme_data_setup();
 $current_options = wp_parse_args(  get_option( 'quality_pro_options', array() ), $quality_pro_options ); ?>
-<div id="latest" class="" style="display: block">
+<div class="latest" style="display: block">
 <!--    <div class="row">-->
         <div class="col-md-4 col-sm-6 latest-property">
             <a href="#" class="latest-property-item-link">
@@ -53,4 +53,39 @@ $current_options = wp_parse_args(  get_option( 'quality_pro_options', array() ),
             <img class="latest-house" src="http://localhost:8888/wp-content/uploads/2015/09/64-marshal-ave.jpg">
         </div>
 <!--    </div>-->
+</div>
+<div class="website-features">
+    <div class="website-feature-content">
+        <div class="website-feature-title">
+            <img class="website-feature-title-img" src="/wp-content/themes/quality/images/green-energy.png"/>
+            <h4>Green Energy</h4>
+        </div>
+        <div class="website-feature-text">
+            <p>
+                We provide information about environmental friendly choices in houses and try to help users live in a greener life
+            </p>
+        </div>
+    </div>
+    <div class="website-feature-content">
+        <div class="website-feature-title">
+            <img class="website-feature-title-img" src="/wp-content/themes/quality/images/livable-community.png"/>
+            <h4>Livable Community</h4>
+        </div>
+        <div class="website-feature-text">
+            <p>
+                Users can find out a community that is most suitable for them based on the broad community information provided by us
+            </p>
+        </div>
+    </div>
+    <div class="website-feature-content">
+        <div class="website-feature-title">
+            <img class="website-feature-title-img" src="/wp-content/themes/quality/images/energy-measurement.png"/>
+            <h4>Energy Measurement</h4>
+        </div>
+        <div class="website-feature-text">
+            <p>
+                User can know the efficiency of different products in the house so that they can choose the proper one for reducing energy consumption
+            </p>
+        </div>
+    </div>
 </div>
---
+++ wp-content/themes/quality/index-threeFeatures.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rachel
+ * Date: 21/09/15
+ * Time: 21:24
+ */
